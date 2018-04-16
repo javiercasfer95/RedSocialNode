bc6ad05efbdbb47bab2fc4b45a77a85a8e104df6
Javier Castro <javiercasfer95@gmail.com>
2018-04-16 12:02:00 +0200

    Empezando con el registrarse

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -4,13 +4,31 @@ module.exports = function (app, swig, gestorBD) {
     });
 
     app.get("/registrarse", function (req, res) {
-        var respuesta = swig.renderFile('views/bregistro.html', {});
+        var respuesta = swig.renderFile('views/signup.html', {});
         res.send(respuesta);
     });
-    app.post('/usuario', function (req, res) {
+    app.post('/registrarse', function (req, res) {
+        var email = req.body.email;
+        //Comprobar que el email no existe en el sistema
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+
+            } else {
+                for(i=0; i < usuarios.length; i++) {
+                    if (usuarios[i].email == email) {
+                        res.redirect("/registrarse" + "?mensaje=Ya existe un usuario con ese email" + "&tipoMensaje=alert-danger");
+                        break
+                    }
+                }
+            }
+
+        });
+
         var seguro = app.get("crypto").createHmac('sha256', app.get('clave')).update(req.body.password).digest('hex');
 
         var usuario = {
+            nombre: req.body.nombre,
+            apellidos: req.body.apellidos,
             email: req.body.email,
             password: seguro
         }
@@ -21,7 +39,7 @@ module.exports = function (app, swig, gestorBD) {
             } else {
                 //res.send('Usuario Insertado ' + id);
                 //res.redirect("/tienda");
-                res.redirect("/Tienda?mensaje=Nuevo usuario registrado");
+                res.redirect("/listUsers?mensaje=Nuevo usuario registrado");
             }
         });
     });
@@ -53,5 +71,27 @@ module.exports = function (app, swig, gestorBD) {
     app.get('/desconectarse', function (req, res) {
         req.session.usuario = null;
         res.send("Usuario desconectado");
-    })
+    });
+
+    //Listar todos los usuarios de la aplicacion
+    app.get("/listUsers", function (req, res) {
+        //var seguro = app.get("crypto").createHmac('sha256', app.get('clave')).update(req.body.password).digest('hex');
+       // var criterio = {email: req.body.email, password: seguro}
+
+        gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                //req.session.usuario = null;
+                //res.send("No identificado: ");
+                //res.redirect("/identificarse" + "?mensaje=Email o password incorrecto" + "&tipoMensaje=alert-danger ");
+            } else {
+
+                var usuarioSesion = req.session.usuario;
+
+                var respuesta = swig.renderFile('views/list.html', {usuario: usuarioSesion, usuarios : usuarios});
+                res.send(respuesta);
+
+            }
+
+        });
+    });
 };
