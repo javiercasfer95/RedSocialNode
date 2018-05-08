8c1dfeaa70b778bd7194ca82d4687b2fa8e8eef6
Javier C. f(z) <javitxu1995@gmail.com>
2018-05-08 19:29:05 +0200

    Mas ids de pruebas

--- app.js
+++ app.js
@@ -93,62 +93,9 @@ app.use("/listUsers", routerUsuarioSession);
 app.use("/peticion*", routerUsuarioSession);
 app.use("/amigo*", routerUsuarioSession);
 app.use("/admin*", routerUsuarioSession);
-app.use("/canciones/agregar", routerUsuarioSession);
-app.use("/publicaciones", routerUsuarioSession);
-app.use("/cancion/comprar", routerUsuarioSession);
-app.use("/compras", routerUsuarioSession);
 
 
 
-//routerUsuarioAutor
-var routerUsuarioAutor = express.Router();
-routerUsuarioAutor.use(function (req, res, next) {
-    console.log("routerUsuarioAutor");
-    var path = require('path');
-    var id = path.basename(req.originalUrl);   // Cuidado porque req.params no funciona   // en el router si los params van en la URL
-    gestorBD.obtenerCanciones({_id: mongo.ObjectID(id)}, function (canciones) {
-        console.log(canciones[0]);
-        if (canciones[0].autor == req.session.usuario) {
-            next();
-        } else {
-            res.redirect("/home");
-        }
-    })
-});
-//Aplicar routerUsuarioAutor
-app.use("/cancion/modificar", routerUsuarioAutor);
-app.use("/cancion/eliminar", routerUsuarioAutor);
-
-
-//routerAudios
-var routerAudios = express.Router();
-routerAudios.use(function (req, res, next) {
-    console.log("routerAudios");
-    var path = require('path');
-    var idCancion = path.basename(req.originalUrl, '.mp3');
-    gestorBD.obtenerCanciones({_id: mongo.ObjectID(idCancion)}, function (canciones) {
-        if (canciones[0].autor == req.session.usuario) {
-            next();
-        } else {
-            //res.redirect("/home");
-            var criterio = {
-                usuario: req.session.usuario,
-                cancionId: mongo.ObjectID(idCancion)
-            };
-            gestorBD.obtenerCompras(criterio, function (compras) {
-                if (compras != null && compras.length > 0) {
-                    next();
-                } else {
-                    res.redirect("/home");
-                }
-            });
-        }
-    })
-
-});
-//Aplicar routerAudios
-app.use("/audios/", routerAudios);
-app.use(express.static('public'));
 
 //FIN ROUTERS
 
